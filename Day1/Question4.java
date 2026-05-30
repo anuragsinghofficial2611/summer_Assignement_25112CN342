29eacf714bf4c8dac449fc888cc35e90f4de9c17
Anurag Singh <anuragsinghofficial2611@gmail.com>
2026-05-30 22:14:33 +0530

    added 4th question

--- Day1/Question4.java
+++ Day1/Question4.java
@@ -1,3 +1,16 @@
+//Write a program to Count digits in a number.
+import java.util.Scanner ;
 public class Question4 {
-    
+    public static void main(String[] args){
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0 ;
+        if(n < 0) n = 0-n;
+        while(n>0){
+            n/=10;
+            count++;
+        }
+        System.out.println("Enter number has " + count + " digits");
+    }
 }
