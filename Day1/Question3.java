cf8ab616f51bedf5802ca3711a4367e4b9a5b580
Anurag Singh <anuragsinghofficial2611@gmail.com>
2026-05-30 21:32:40 +0530

    Question 3

--- Day1/Question3.java
+++ Day1/Question3.java
@@ -1,3 +1,14 @@
+//Write a program to Find factorial of a number. 
+import java.util.Scanner;
 public class Question3 {
-    
+    public static void main(String[] args){
+        System.out.print("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+        for(int i = 1 ; i <=n ; i++){
+            fact*=i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+    }
 }
