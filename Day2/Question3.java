4c38ddcbc08eec2a6b3bc943361e4d31733e6ac2
Anurag Singh <anuragsinghofficial2611@gmail.com>
2026-06-03 21:24:41 +0530

    day2 done

--- Day2/Question3.java
+++ Day2/Question3.java
@@ -1,5 +1,16 @@
-package Day2;
-
+// Write a program to Find product of digits. 
+import java.util.Scanner;
 public class Question3 {
-    
+    public static void main(String[] args) {
+        int product = 1;
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n > 0) {
+            product *= n % 10;
+            n = n / 10;
+        }
+        System.out.println("Product of digits is: " + product);
+        sc.close();
+    }
 }
--- Day2/Question4.java
+++ Day2/Question4.java
@@ -1,5 +1,30 @@
-package Day2;
-
+//Write a program to Check whether a number is palindrome
+import java.util.Scanner;
 public class Question4 {
-    
+    public static void main(String args[]){
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int digits = 0;
+        int num_copy = num;
+        if (num_copy == 0) {
+            digits = 1;
+        }
+        while(num_copy>0){
+            num_copy /= 10;
+            digits++;
+        }
+        int[] arr = new int[digits];
+        for(int i = arr.length-1 ;i >= 0 ; i--){
+            arr[i] = num%10;
+            num /= 10;
+        }
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] != arr[arr.length - i - 1]){
+                System.out.println("Not a Palindrome");
+                return;
+            }
+        }
+        System.out.println("Number is Palindrome");
+    }
 }
